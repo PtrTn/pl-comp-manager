8a8a76152385869599a136f60481a1b2469c2cfb
Peter Ton <mail@peterton.nl>
2018-02-10 23:32:24 +0100

    Update factory test data

--- database/factories/LifterFactory.php
+++ database/factories/LifterFactory.php
@@ -5,15 +5,15 @@ use Faker\Generator as Faker;
 
 $factory->define(Lifter::class, function (Faker $faker) {
     return [
-        'naam' => str_random(10),
+        'naam' => str_random(rand(5, 10)) . ' ' . str_random(rand(5, 10)),
         'squat1' => rand(100, 300),
         'squat2' => rand(100, 300),
-        'squat3' => rand(100, 300),
+        'squat3' => null,
         'bench1' => rand(100, 300),
         'bench2' => rand(100, 300),
-        'bench3' => rand(100, 300),
+        'bench3' => null,
         'deadlift1' => rand(100, 300),
         'deadlift2' => rand(100, 300),
-        'deadlift3' => rand(100, 300),
+        'deadlift3' => null,
     ];
 });
